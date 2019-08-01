22ec94639b8f23c8303b578fe5607a4692d4d840
soran <soranvibritannia@hotmail.com>
2019-08-01 01:54:51 -0600

    primer comimit

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+   public function welcome()
+   {
+   	$a = 5;
+   	$b = 10;
+   	$c = $a+$b;
+   	return view('welcome');
+   }
+}
---
+++ routes/web.php
@@ -0,0 +1,6 @@
+<?php
+Route::get('/', 'TestController@welcome');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
